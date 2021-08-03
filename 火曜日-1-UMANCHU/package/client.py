2dcfab5994baeaf72a48bcad20fe73bfc5e6d936
Yoshiaki Sano <e205723@ie.u-ryukyu.ac.jp>
2021-08-03 12:43:30 +0900

    a

--- 火曜日-1-UMANCHU/package/client.py
+++ 火曜日-1-UMANCHU/package/client.py
@@ -253,7 +253,15 @@ class UserInterface():
         PORT = 49152   # Port to listen on (non-privileged ports are > 1023)
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST, 49152))
+            ok = False
+            while not OK:
+                try:
+                    s.bind((HOST, 49152))
+                    sleep(0.01)
+                    ok = True
+                except:
+                    pass
+
             s.listen()
             conn, addr = s.accept()
             with conn:
